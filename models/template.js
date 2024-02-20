143199dcd44794502d11332a77e8dfae57dd6f33
peepee406 <71884583+peepee406@users.noreply.github.com>
2024-02-20 13:35:22 +0530

    Create template.js

---
+++ models/template.js
@@ -0,0 +1,10 @@
+// models/template.js
+
+const mongoose = require('mongoose');
+
+const templateSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  roll: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Template', templateSchema);
